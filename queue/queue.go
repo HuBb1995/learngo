a5df848814393d9b604a1c1bbdcf62b0800c80df
HuBb1995 <846249361@qq.com>
2021-08-02 08:36:10 +0800

    对 queue.go queue_test.go 生成godoc文档

--- queue/queue.go
+++ queue/queue.go
@@ -1,17 +1,22 @@
 package queue
 
+// An FIFO queue
 type Queue []int
 
+// Push Pushes element into queue
+//	e.g. q.push(123)
 func (q *Queue) Push(v int) {
 	*q = append(*q, v)
 }
 
+// Pops element from queue
 func (q *Queue) Pop() int {
 	head := (*q)[0]
 	*q = (*q)[1:]
 	return head
 }
 
+// Returns if the queue is empty or not
 func (q *Queue) IsEmpty() bool {
 	return len(*q) == 0
 }
